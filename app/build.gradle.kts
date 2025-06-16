209c6f77bffff14acd8195f1b28fe6cb88d595f1
Wybie <lyrncofyn@gmail.com>
2025-06-17 04:16:15 +0800

    Add google accompanist permissions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,6 +85,7 @@ dependencies {
     implementation(libs.play.services.auth)
     implementation(libs.play.services.maps)
     implementation(libs.play.services.location)
+    implementation(libs.accompanist.permissions)
 
     implementation(libs.glide)
 
